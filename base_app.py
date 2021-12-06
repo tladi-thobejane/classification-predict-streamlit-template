eeca93ccb9a88a7f911c7e4c234c585964ac84a1
ririzqah <90268312+ririzqah@users.noreply.github.com>
2021-12-06 06:31:19 +0200

    Add files via upload

--- base_app.py
+++ base_app.py
@@ -24,9 +24,14 @@
 # Streamlit dependencies
 import streamlit as st
 import joblib,os
+from annotated_text import annotated_text
+from sklearn.metrics import accuracy_score
+import plotly.figure_factory as ff
 
 # Data dependencies
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Vectorizer
 news_vectorizer = open("resources/tfidfvect.pkl","rb")
@@ -41,27 +46,49 @@ def main():
 
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
-	st.title("Tweet Classifer")
-	st.subheader("Climate change tweet classification")
+	st.title("Climate Change Classifier :earth_africa:")
+	st.header("_Where do we stand on climate change?_")
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
+	options = ["Information", "Prediction", "Predict data", "About Model"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
 	# Building out the "Information" page
-	if selection == "Information":
-		st.info("General Information")
-		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
-
+	if selection == 'Predict data':
+		st.markdown("Below you can have a view of the data we used to predict our model")
 		st.subheader("Raw Twitter data and label")
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
+#st.write(center_info_data)
+	# Building out the "About model" page
+	if selection == 'About Model':
+		st.info('Model used: Linear Support Vector Classifier(SVC) model')
+		st.subheader("Why this model?")
+
+		st.markdown("For this classifier we used a Linear Support Vector Classifier model. The goal of this model is to fit to the provided data, returning a 'best fit' hyperplane that categorizes your data. After testing several multiclass predictor models we concluded that the Linear SVC gives us the higest accuracy in term of predicting someone's view on climate change based on their tweets.")
+		st.info('More about Linear SVM models: https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html')
+	#Building out the "Predict Data" page
+	if selection == "Information":
+		st.subheader("General Information")
+		# You can read a markdown file from supporting resources folder
+		st.markdown("With our **Linear Support Vector Classification Model** you will be able to predict whether or not a person believes in climate change with 74% accuracy.")
+		st.markdown("It's fairly simple to predict, just head over to the **Prediction** option and follow the instructions")
+		st.markdown("")
+		st.markdown("")
+		st.markdown(':memo: Instructions:')
+		st.markdown("1. Enter your preferred text/tweet in the **Enter text** box\n2. Click on the **Classify** button")
+		st.markdown("Your answer will show as **Text Categorized as: [X]**. The 'X' defines the number to each sentiment.")
+		st.markdown("")
+		st.markdown("**💬 Sentiments:**")
+		st.markdown("[-1] Anti: the tweet does not believe in man-made climate change\n")
+		st.markdown("[0] Neutral: the tweet neither supports nor refutes the belief of man-made climate change")
+		st.markdown("[1] Pro: the tweet supports the belief of man-made climate change")
+		st.markdown("[2] News: the tweet links to factual news about climate change")
 	# Building out the predication page
 	if selection == "Prediction":
-		st.info("Prediction with ML Models")
+		st.text("Classify whether or not a person believes in climate change, based on their tweet.")
 		# Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
 
